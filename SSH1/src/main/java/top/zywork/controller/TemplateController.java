a29e519f3a940cda54938ba9403317a71d2942fd
WangGenshen <847315251@qq.com>
2017-08-24 20:53:56 +0800

    1、zywork-common中使用slf4j日志工具 2、修改log4j.properties配置

--- SSH1/src/main/java/top/zywork/controller/TemplateController.java
+++ SSH1/src/main/java/top/zywork/controller/TemplateController.java
@@ -1,6 +1,9 @@
 package top.zywork.controller;
 
 import com.opensymphony.xwork2.ActionSupport;
+import org.hibernate.sql.Template;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import top.zywork.common.EncryptUtils;
 import top.zywork.dto.TemplateDTO;
 import top.zywork.service.TemplateService;
@@ -16,6 +19,8 @@ import top.zywork.service.TemplateService;
 public class TemplateController extends ActionSupport {
     private static final long serialVersionUID = 1647679288455528889L;
 
+    private Logger logger = LoggerFactory.getLogger(TemplateController.class);
+
     private TemplateService templateService;
 
     public void setTemplateService(TemplateService templateService) {
@@ -23,6 +28,8 @@ public class TemplateController extends ActionSupport {
     }
 
     public String execute() {
+        logger.info("template******");
+        logger.debug("你好！");
         TemplateDTO templateDTO = new TemplateDTO();
         templateDTO.setName("test");
         templateDTO.setPassword(EncryptUtils.md5("123456"));
