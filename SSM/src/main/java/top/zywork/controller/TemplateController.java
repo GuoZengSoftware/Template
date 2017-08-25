077748a050e9a875b4ed9daddf094fd3e7634b8a
WangGenshen <847315251@qq.com>
2017-08-25 17:32:53 +0800

    增加SSM，MyBatis使用配置文件的方式，使用logback日志记录工具

---
+++ SSM/src/main/java/top/zywork/controller/TemplateController.java
@@ -0,0 +1,45 @@
+package top.zywork.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import top.zywork.common.EncryptUtils;
+import top.zywork.dto.TemplateDTO;
+import top.zywork.service.TemplateService;
+
+import javax.annotation.Resource;
+
+/**
+ * Template测试控制器类<br />
+ * 创建于2017-08-24
+ *
+ * @author 王振宇
+ * @version 1.0
+ *
+ */
+@Controller
+@RequestMapping("/template")
+public class TemplateController {
+
+    private Logger logger = LoggerFactory.getLogger(TemplateController.class);
+
+    private TemplateService templateService;
+
+    @Resource
+    public void setTemplateService(TemplateService templateService) {
+        this.templateService = templateService;
+    }
+
+    @RequestMapping(value = "ssm", method = RequestMethod.GET)
+    public String add() {
+        logger.info("template******");
+        logger.debug("你好！");
+        TemplateDTO templateDTO = new TemplateDTO();
+        templateDTO.setName("test");
+        templateDTO.setPassword(EncryptUtils.md5("123456"));
+        templateService.save(templateDTO);
+        return "index";
+    }
+}
---
+++ SSM/src/main/java/top/zywork/service/impl/TemplateServiceImpl.java
@@ -0,0 +1,74 @@
+package top.zywork.service.impl;
+
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
+import org.springframework.stereotype.Service;
+import top.zywork.dao.TemplateDAO;
+import top.zywork.dos.TemplateDO;
+import top.zywork.dto.PagerDTO;
+import top.zywork.dto.TemplateDTO;
+import top.zywork.query.PageQuery;
+import top.zywork.query.StatusQuery;
+import top.zywork.service.TemplateService;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * Template服务实现类<br />
+ * 创建于2017-08-24
+ *
+ * @author 王振宇
+ * @version 1.0
+ */
+@Service
+public class TemplateServiceImpl implements TemplateService {
+
+    private TemplateDAO templateDAO;
+
+    @Resource
+    public void setTemplateDAO(TemplateDAO templateDAO) {
+        this.templateDAO = templateDAO;
+    }
+
+    @Override
+    public void save(TemplateDTO templateDTO) {
+        Mapper mapper = new DozerBeanMapper();
+        templateDAO.save(mapper.map(templateDTO, TemplateDO.class));
+    }
+
+    @Override
+    public void remove(TemplateDTO templateDTO) {
+
+    }
+
+    @Override
+    public void remove(Long id) {
+
+    }
+
+    @Override
+    public void update(TemplateDTO templateDTO) {
+
+    }
+
+    @Override
+    public void updateStatus(StatusQuery statusQuery) {
+
+    }
+
+    @Override
+    public TemplateDTO getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<TemplateDTO> listAll() {
+        return null;
+    }
+
+    @Override
+    public PagerDTO listPage(PageQuery pageQuery) {
+        return null;
+    }
+}
