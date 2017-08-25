3ccb2ff9f7395e8aff8229c9432003ff24b92704
WangGenshen <847315251@qq.com>
2017-08-25 14:32:03 +0800

    添加注解方式的SSH2

---
+++ SSH2/src/main/java/top/zywork/controller/TemplateController.java
@@ -0,0 +1,46 @@
+package top.zywork.controller;
+
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.convention.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import top.zywork.common.EncryptUtils;
+import top.zywork.dto.TemplateDTO;
+import top.zywork.service.TemplateService;
+
+import javax.annotation.Resource;
+
+/**
+ * Template测试控制器类<br />
+ * 创建于2017-08-24
+ *
+ * @author 王振宇
+ * @version 1.0
+ *
+ */
+@Controller
+@ParentPackage(value = "base-package")
+@Namespace(value = "/template")
+public class TemplateController extends ActionSupport {
+    private static final long serialVersionUID = 1647679288455528889L;
+
+    private Logger logger = LoggerFactory.getLogger(TemplateController.class);
+
+    private TemplateService templateService;
+
+    @Resource
+    public void setTemplateService(TemplateService templateService) {
+        this.templateService = templateService;
+    }
+
+    @Action(value = "ssh2", results = {@Result(name = "success", location = "/index.jsp")})
+    public String execute() {
+        logger.info("template******");
+        TemplateDTO templateDTO = new TemplateDTO();
+        templateDTO.setName("test");
+        templateDTO.setPassword(EncryptUtils.md5("123456"));
+        templateService.save(templateDTO);
+        return SUCCESS;
+    }
+}
---
+++ SSH2/src/main/java/top/zywork/dao/impl/TemplateDAOImpl.java
@@ -0,0 +1,64 @@
+package top.zywork.dao.impl;
+
+import org.springframework.stereotype.Repository;
+import top.zywork.dao.AbstractBaseDAO;
+import top.zywork.dao.TemplateDAO;
+import top.zywork.dos.TemplateDO;
+import top.zywork.query.PageQuery;
+import top.zywork.query.StatusQuery;
+
+import java.util.List;
+
+/**
+ * TemplateDAO的实现类<br />
+ * 创建于2017-08-24
+ * @author 王振宇
+ * @version 1.0
+ */
+@Repository
+public class TemplateDAOImpl extends AbstractBaseDAO implements TemplateDAO {
+    @Override
+    public void save(TemplateDO templateDO) {
+        getSessionFactory().getCurrentSession().save(templateDO);
+    }
+
+    @Override
+    public void remove(TemplateDO templateDO) {
+
+    }
+
+    @Override
+    public void remove(Long id) {
+
+    }
+
+    @Override
+    public void update(TemplateDO templateDO) {
+
+    }
+
+    @Override
+    public void updateStatus(StatusQuery statusQuery) {
+
+    }
+
+    @Override
+    public TemplateDO getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<TemplateDO> listAll() {
+        return null;
+    }
+
+    @Override
+    public List<TemplateDO> listPage(PageQuery pageQuery) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+}
