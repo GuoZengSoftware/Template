3fe34b1e3a3c3285b63081b9cfc71a8ab7f7a187
WangGenshen <847315251@qq.com>
2017-09-04 11:27:50 +0800

    修改加密工具类，加入盐值增加加密算法的可靠性

--- zywork-common/src/main/java/top/zywork/common/EncryptUtils.java
+++ zywork-common/src/main/java/top/zywork/common/EncryptUtils.java
@@ -9,11 +9,12 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 /**
- * 加密工具类
- * 创建于2017-08-15
+ * 加密工具类<br />
+ * 创建于2017-08-15<br />
+ * 修改于2017-09-04，增加salt<br />
  *
  * @author 王振宇
- * @version 1.0
+ * @version 1.1
  * @see top.zywork.enums.AlgorithmEnum
  */
 public class EncryptUtils {
@@ -27,6 +28,22 @@ public class EncryptUtils {
         String encryptStr = null;
         try {
             encryptStr = oneWayEncrypt(str, AlgorithmEnum.MD5.getValue());
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return encryptStr;
+    }
+
+    /**
+     * md5加密
+     * @param str 明文
+     * @param salt 盐值
+     * @return 使用md5加密算法并加入盐值加密得到的密文
+     */
+    public static String md5(String str, String salt) {
+        String encryptStr = null;
+        try {
+            encryptStr = oneWayEncrypt(str, salt, AlgorithmEnum.MD5.getValue());
         } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
             e.printStackTrace();
         }
@@ -49,17 +66,46 @@ public class EncryptUtils {
     }
 
     /**
+     * sha1加密
+     * @param str 明文
+     * @param salt 盐值
+     * @return 使用sha1加密算法并加入盐值加密得到的密文
+     */
+    public static String sha1(String str, String salt) {
+        String encryptStr = null;
+        try {
+            encryptStr = oneWayEncrypt(str, "",  AlgorithmEnum.SHA1.getValue());
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return encryptStr;
+    }
+
+    /**
      * 单向加密，使用Base64编码
      * @param str 需要加密的明文
      * @param type 加密或消息摘要算法名称
      * @return 通过指定加密算法得到的密文
-     * @throws NoSuchAlgorithmException
-     * @throws UnsupportedEncodingException
+     * @throws NoSuchAlgorithmException 未知的算法
+     * @throws UnsupportedEncodingException 不支持的编码方式
      */
     public static String oneWayEncrypt(String str, String type) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        return oneWayEncrypt(str, "", type);
+    }
+
+    /**
+     * 单向加密，使用Base64编码
+     * @param str 需要加密的明文
+     * @param salt 加密所使用的盐值
+     * @param type 加密或消息摘要算法名称
+     * @return 通过指定加密算法和盐值得到的密文
+     * @throws NoSuchAlgorithmException 未知的算法
+     * @throws UnsupportedEncodingException 不支持的编码方式
+     */
+    public static String oneWayEncrypt(String str, String salt, String type) throws NoSuchAlgorithmException, UnsupportedEncodingException {
         MessageDigest md = MessageDigest.getInstance(type);
         BASE64Encoder encoder = new BASE64Encoder();
-        byte[] bytes = str.getBytes(CharsetEnum.UTF8.getValue());
+        byte[] bytes = (str + salt).getBytes(CharsetEnum.UTF8.getValue());
         md.update(bytes);
         return encoder.encode(md.digest(bytes));
     }
